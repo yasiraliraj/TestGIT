076aa669bf48bdba763ee66de60526032b65860a
Wasif Younas <wasif788@gmail.com>
2012-10-31 15:52:22 +0500

    Thanks!

--- src/git/test/testgit/MainActivity.java
+++ src/git/test/testgit/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         // aa
         // bbb ccc
-        // chal ho ja yaar
+        // chal ho ja yaar. ok ho gaya..
     }
 
     @Override
